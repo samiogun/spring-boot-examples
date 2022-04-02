72c00c5c84a660018024759e92117f10ca95b6c3
OGÜN <samiogunersun@gmail.com>
2022-04-02 20:02:19 +0300

    Revert "First commit"
    This reverts commit 7bbf25ef70cf3167614c53176572bf0b7a994d8a.

--- spring-datajpa-postgresql/src/main/java/org/ersun/repository/AddressRepository.java
+++
@@ -1,9 +0,0 @@
-package org.ersun.repository;
-
-import org.ersun.model.address.Address;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AddressRepository extends JpaRepository<Address , Long> {
-}
