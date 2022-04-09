c9bb0edb12ecbd2b9f4443555a19bf843eb6edd0
OGÜN <samiogunersun@gmail.com>
2022-04-09 22:49:36 +0300

    Added SpringBootServletInitializer extend to SpringBootDocApplication.

--- spring-data-api-doc/src/main/java/org/ersun/SpringBootDocApplication.java
+++ spring-data-api-doc/src/main/java/org/ersun/SpringBootDocApplication.java
@@ -2,12 +2,18 @@ package org.ersun;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootDocApplication {
+public class SpringBootDocApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootDocApplication.class,args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SpringBootDocApplication.class);
+    }
 }
