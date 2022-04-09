36d29e83fe700b2447969cb2bf141416560f4403
OGÜN <samiogunersun@gmail.com>
2022-04-09 21:15:19 +0300

    Manuel deploy to Tomcat OK version. App is workig fine on Docker Tomcat Container.

--- spring-datajpa-postgresql/src/main/java/org/ersun/PostgresqlApplication.java
+++ spring-datajpa-postgresql/src/main/java/org/ersun/PostgresqlApplication.java
@@ -2,19 +2,20 @@ package org.ersun;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
-public class PostgresqlApplication{
+@SpringBootApplication/*(exclude = SecurityAutoConfiguration.class)*/
+public class PostgresqlApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(PostgresqlApplication.class,args);
     }
 
-//    @Override
-//    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-//        return builder.sources(PostgresqlApplication.class);
-//    }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(PostgresqlApplication.class);
+    }
 
 
 }
